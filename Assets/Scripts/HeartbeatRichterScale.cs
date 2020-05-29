cfa7144608265374ebde0fa97ed555547e43fae8
Jing <jtan1993@gmail.com>
2020-05-29 17:13:58 +0100

    added stars texturesheet

---
+++ Assets/Scripts/HeartbeatRichterScale.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartbeatRichterScale : MonoBehaviour
+{
+    private float yPos;
+    [SerializeField] private int interval = 3;
+    [SerializeField] private float yPosMax = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.frameCount % interval == 0)
+        {
+            yPos = Random.Range(0, yPosMax);
+            this.gameObject.transform.position = new Vector3(1, yPos, 1);
+        }
+    }
+}
